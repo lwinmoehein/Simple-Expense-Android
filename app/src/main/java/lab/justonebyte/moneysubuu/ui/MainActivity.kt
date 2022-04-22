d18ffcae1048910ddd7a196b2c5c9bdcb41a7eca
lwinmoehein <dev.lwinmoehein@gmail.com>
2022-04-22 18:55:34 +0630

    add drawer

--- app/src/main/java/lab/justonebyte/moneysubuu/ui/MainActivity.kt
+++ app/src/main/java/lab/justonebyte/moneysubuu/ui/MainActivity.kt
@@ -3,28 +3,22 @@ package lab.justonebyte.moneysubuu.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
 import lab.justonebyte.moneysubuu.ui.theme.MoneySuBuuTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+
         setContent {
-            MoneySuBuuTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            SuBuuApp()
         }
     }
 }
--- app/src/main/java/lab/justonebyte/moneysubuu/ui/SuBuuApp.kt
+++ app/src/main/java/lab/justonebyte/moneysubuu/ui/SuBuuApp.kt
@@ -1,51 +1,75 @@
 package lab.justonebyte.moneysubuu.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import kotlinx.coroutines.launch
 import lab.justonebyte.moneysubuu.ui.theme.MoneySuBuuTheme
-//
-//@Composable
-//fun SuBuuApp(
-//) {
-//    MoneySuBuuTheme {
-//        ProvideWindowInsets {
-//            val systemUiController = rememberSystemUiController()
-//            SideEffect {
-//                systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = false)
-//            }
-//
-//            val navController = rememberNavController()
-//            val coroutineScope = rememberCoroutineScope()
-//            // This top level scaffold contains the app drawer, which needs to be accessible
-//            // from multiple screens. An event to open the drawer is passed down to each
-//            // screen that needs it.
-//            val scaffoldState = rememberScaffoldState()
-//
-//            val navBackStackEntry by navController.currentBackStackEntryAsState()
-//            val currentRoute = navBackStackEntry?.destination?.route ?: MainDestinations.HOME_ROUTE
-//            Scaffold(
-//                scaffoldState = scaffoldState,
-//                drawerContent = {
-//                    AppDrawer(
-//                        currentRoute = currentRoute,
-//                        navigateToHome = { navController.navigate(MainDestinations.HOME_ROUTE) },
-//                        navigateToSettings = { navController.navigate(MainDestinations.SETTINGS_ROUTE) },
-//                        closeDrawer = { coroutineScope.launch { scaffoldState.drawerState.close() } }
-//                    )
-//                }
-//            ) {
-//                SuBuuNavGraph(
-//                    navController = navController,
-//                    scaffoldState = scaffoldState
-//                )
-//            }
-//        }
-//    }
-//}
+
+@Composable
+fun SuBuuApp(
+) {
+    MoneySuBuuTheme {
+        ProvideWindowInsets {
+            val systemUiController = rememberSystemUiController()
+            SideEffect {
+                systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = false)
+            }
+
+            val navController = rememberNavController()
+            val coroutineScope = rememberCoroutineScope()
+            // This top level scaffold contains the app drawer, which needs to be accessible
+            // from multiple screens. An event to open the drawer is passed down to each
+            // screen that needs it.
+            val scaffoldState = rememberScaffoldState()
+
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentRoute = navBackStackEntry?.destination?.route ?: MainDestinations.HOME_ROUTE
+            Scaffold(
+                topBar = {
+                    Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start) {
+                        IconButton(onClick = {
+                            if(scaffoldState.drawerState.isOpen){
+                                coroutineScope.launch { scaffoldState.drawerState.close() }
+                            }else{
+                                coroutineScope.launch { scaffoldState.drawerState.open() }
+                            }
+                        }) {
+                            Icon(imageVector = Icons.Filled.Menu, contentDescription = "open menu")
+                        }
+                    }
+                },
+                scaffoldState = scaffoldState,
+                drawerContent = {
+                    AppDrawer(
+                        currentRoute = currentRoute,
+                        navigateToHome = { navController.navigate(MainDestinations.HOME_ROUTE) },
+                        navigateToSettings = { navController.navigate(MainDestinations.SETTINGS_ROUTE) },
+                        closeDrawer = { coroutineScope.launch { scaffoldState.drawerState.close() } }
+                    )
+                }
+            ) {
+                SuBuuNavGraph(
+                    navController = navController,
+                    scaffoldState = scaffoldState
+                )
+            }
+        }
+    }
+}
