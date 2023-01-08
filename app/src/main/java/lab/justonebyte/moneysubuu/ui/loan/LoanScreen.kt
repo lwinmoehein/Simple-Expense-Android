8d90db78634c2569c2c3a4656327a85b4f59f1c3
lwinmoehein <dev.lwinmoehein@gmail.com>
2023-01-08 11:09:53 +0630

    - add loan , budget screens

---
+++ app/src/main/java/lab/justonebyte/moneysubuu/ui/loan/LoanScreen.kt
@@ -0,0 +1,19 @@
+package lab.justonebyte.moneysubuu.ui.loan
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import lab.justonebyte.moneysubuu.ui.appContentPadding
+
+@Composable
+fun LoanScreen(
+    openDrawer:()->Unit,
+){
+    Column (
+        modifier = Modifier.padding(appContentPadding)
+    ){
+        Text(text = "Loan")
+    }
+}
