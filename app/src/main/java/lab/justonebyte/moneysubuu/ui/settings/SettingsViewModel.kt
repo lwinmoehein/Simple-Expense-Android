de51ef5c6d966a16c50210b541e10b01f9bd6d49
lwinmoehein <dev.lwinmoehein@gmail.com>
2023-01-26 01:22:03 +0630

    - add test setting viewmodel

---
+++ app/src/main/java/lab/justonebyte/moneysubuu/ui/settings/SettingsViewModel.kt
@@ -0,0 +1,79 @@
+package lab.justonebyte.moneysubuu.ui.settings
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import lab.justonebyte.moneysubuu.data.CategoryRepository
+import lab.justonebyte.moneysubuu.data.SettingPrefRepository
+import lab.justonebyte.moneysubuu.model.BalanceType
+import lab.justonebyte.moneysubuu.model.Currency
+import lab.justonebyte.moneysubuu.model.TransactionCategory
+import lab.justonebyte.moneysubuu.ui.components.SnackBarType
+import javax.inject.Inject
+
+data class SettingUiState(
+    val selectedCurrency: Currency = Currency.Kyat,
+    val defaultBalanceType: BalanceType = BalanceType.MONTHLY,
+    val currentSnackBar : SnackBarType? = null
+)
+
+@HiltViewModel
+class SettingsViewModel @Inject constructor(
+    private val settingRepository: SettingPrefRepository
+): ViewModel() {
+    private val _viewModelUiState = MutableStateFlow(
+        SettingUiState()
+    )
+    val viewModelUiState: StateFlow<SettingUiState>
+        get() = _viewModelUiState
+
+
+    init {
+        viewModelScope.launch {
+            launch { collectSelectedCurrency() }
+            launch { collectSelectedCurrency() }
+        }
+    }
+    private suspend fun collectSelectedCurrency(){
+        settingRepository.selectedCurrency.collect{
+            _viewModelUiState.update { uiState->
+                uiState.copy(
+                    selectedCurrency = Currency.getFromValue(it)
+                )
+            }
+        }
+    }
+    private suspend fun collectDefaultBalanceType(){
+        settingRepository.defaultBalanceType.collect{
+            _viewModelUiState.update { uiState->
+                uiState.copy(
+                    defaultBalanceType = when(it){
+                        BalanceType.TOTAL.value->BalanceType.TOTAL
+                        BalanceType.YEARLY.value->BalanceType.YEARLY
+                        BalanceType.DAILY.value->BalanceType.DAILY
+                        else->BalanceType.MONTHLY
+                    }
+                )
+            }
+        }
+    }
+
+
+
+    fun showSnackBar(type: SnackBarType){
+        _viewModelUiState.update {
+            it.copy(currentSnackBar = type)
+        }
+    }
+    fun clearSnackBar(){
+        _viewModelUiState.update {
+            it.copy(currentSnackBar = null)
+        }
+    }
+}
