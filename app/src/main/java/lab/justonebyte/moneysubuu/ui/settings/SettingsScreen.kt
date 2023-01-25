b1a992823c0fee0d174ea12decd822856a06fc7a
lwinmoehein <dev.lwinmoehein@gmail.com>
2023-01-26 01:22:03 +0630

    - add test setting viewmodel

--- app/src/main/java/lab/justonebyte/moneysubuu/ui/settings/SettingsScreen.kt
+++ app/src/main/java/lab/justonebyte/moneysubuu/ui/settings/SettingsScreen.kt
@@ -5,18 +5,27 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import lab.justonebyte.moneysubuu.ui.appContentPadding
+import lab.justonebyte.moneysubuu.ui.category.CategoryViewModel
 
 
 @Composable
 fun SettingsScreen(
     openDrawer:()->Unit,
 ){
+    val settingsViewModel = hiltViewModel<SettingsViewModel>()
+    val settingsUiState by settingsViewModel.viewModelUiState.collectAsState()
+    
    Column (
        modifier = Modifier.padding(appContentPadding)
            ){
-       Text(text = "Settings")
+       Text(text = stringResource(id =  settingsUiState.selectedCurrency.name))
+       Text(text = settingsUiState.defaultBalanceType.name)
    }
 }
