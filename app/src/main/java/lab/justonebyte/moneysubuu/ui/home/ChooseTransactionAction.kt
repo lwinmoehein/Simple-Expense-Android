cb4c37e76a4a59a607aaa3e4ff398362421e513d
lwinmoehein <dev.lwinmoehein@gmail.com>
2023-01-29 10:15:43 +0630

    - stage changes

--- app/src/main/java/lab/justonebyte/moneysubuu/ui/home/ChooseTransactionAction.kt
+++ app/src/main/java/lab/justonebyte/moneysubuu/ui/home/ChooseTransactionAction.kt
@@ -3,7 +3,9 @@ package lab.justonebyte.moneysubuu.ui.home
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -28,19 +30,12 @@ fun ChooseTransactionAction(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         TextButton(
-            modifier = Modifier
-                .fillMaxWidth()
-                .clip(RoundedCornerShape(5.dp))
-                .padding(0.dp)
-                .background(primary)
-            ,
             onClick = {
                 onEditClick()
             }
         ) {
-            Text(text = stringResource(id = R.string.edit_tran),color=MaterialTheme.colors.onPrimary)
+            Text(text = stringResource(id = R.string.edit_tran))
         }
-        Divider(Modifier.height(20.dp), color = Color.Transparent)
         TextButton(
             modifier = Modifier
                 .fillMaxWidth()
@@ -52,7 +47,7 @@ fun ChooseTransactionAction(
                onDeleteClick()
             }
         ) {
-            Text(text = stringResource(id = R.string.delete_tran), color = MaterialTheme.colors.onPrimary)
+            Text(text = stringResource(id = R.string.delete_tran))
         }
     }
 }
