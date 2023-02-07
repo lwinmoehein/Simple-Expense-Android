60028050cc88656b0770c54e431348ade28c1d1a
lwinmoehein <dev.lwinmoehein@gmail.com>
2023-02-07 23:25:09 +0630

    - add categories screen adjustments

---
+++ app/src/main/java/lab/justonebyte/moneysubuu/ui/category/ChooseCategoryActionDialog.kt
@@ -0,0 +1,69 @@
+package lab.justonebyte.moneysubuu.ui.category
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import lab.justonebyte.moneysubuu.R
+import lab.justonebyte.moneysubuu.ui.components.AppAlertDialog
+import lab.justonebyte.moneysubuu.ui.home.AddTransactionAction
+
+@Composable
+fun ChooseCategoryActionDialog(
+    isOpen:Boolean = false,
+    onDeleteCategory:()->Unit,
+    onEditCategory:()->Unit
+){
+    if (isOpen) {
+        AppAlertDialog(
+            title = "What type of transaction?",
+            positiveBtnText =null,
+            negativeBtnText = null,
+            content = {
+                ChooseTransactionAction(
+                    onDeleteCategory = {
+                        onDeleteCategory()
+                    },
+                    onEditCategory = {
+                        onEditCategory()
+                    }
+                )
+            }
+        )
+    }
+}
+
+@Composable
+fun ChooseTransactionAction(
+    onDeleteCategory: () -> Unit,
+    onEditCategory: () -> Unit
+){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {
+                onDeleteCategory()
+            }
+        ) {
+            Text(text =  stringResource(id = R.string.delete_cat))
+        }
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {
+                onEditCategory()
+            }
+        ) {
+            Text(text = stringResource(id = R.string.edit_cat))
+        }
+    }
+
+}
--- app/src/main/java/lab/justonebyte/moneysubuu/ui/category/ManageCategoryScreen.kt
+++ app/src/main/java/lab/justonebyte/moneysubuu/ui/category/ManageCategoryScreen.kt
@@ -1,13 +1,14 @@
 package lab.justonebyte.moneysubuu.ui.category
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.*
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Tab
@@ -20,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.accompanist.pager.*
@@ -28,296 +30,190 @@ import lab.justonebyte.moneysubuu.R
 import lab.justonebyte.moneysubuu.model.TransactionCategory
 import lab.justonebyte.moneysubuu.model.TransactionType
 import lab.justonebyte.moneysubuu.ui.components.*
-import lab.justonebyte.moneysubuu.ui.theme.Red300
+import lab.justonebyte.moneysubuu.ui.home.ChooseTransactionTypeDialog
 import lab.justonebyte.moneysubuu.ui.theme.negativeColor
 import lab.justonebyte.moneysubuu.ui.theme.positiveColor
-import lab.justonebyte.moneysubuu.ui.theme.primary
 
 sealed class CategoryTab(val index:Int,val title:Int){
-    object  Income:CategoryTab(1,R.string.income)
-    object Expense:CategoryTab(2,R.string.expense)
+    object  Income:CategoryTab(0,R.string.income)
+    object Expense:CategoryTab(1,R.string.expense)
 }
-val tabs = listOf(
-    CategoryTab.Income,CategoryTab.Expense
-)
 
-@OptIn(ExperimentalMaterialApi::class)
+
 @ExperimentalPagerApi
 @Composable
 fun CategoryTabs(
-    categoryUiState: CategoryUiState,
-    categoryViewModel: CategoryViewModel,
-    onTabChanged:(CategoryTab)->Unit,
-    type:TransactionType
+    onTabChanged: (CategoryTab) -> Unit,
+    currentCategoryTabIndex: Int
 ) {
 
-    var tabIndex by remember { mutableStateOf(1) }
-    val pagerState = rememberPagerState()
-    val coroutineScope = rememberCoroutineScope()
-    val currentTab:MutableState<CategoryTab> = remember { mutableStateOf(CategoryTab.Income) }
-    val scaffoldState = rememberBottomSheetScaffoldState(
-        bottomSheetState = BottomSheetState(BottomSheetValue.Collapsed)
-    )
+    val categoryTabs = listOf(CategoryTab.Income,CategoryTab.Expense)
 
-    LaunchedEffect(pagerState) {
-        // Collect from the pager state a snapshotFlow reading the currentPage
-        snapshotFlow { pagerState.currentPage }.collect { page ->
-            val currentTab = when(pagerState.currentPage+1){
-                CategoryTab.Income.index-> CategoryTab.Income
-                else-> CategoryTab.Expense
-            }
-            onTabChanged(currentTab)
-        }
-    }
-
-    Column(
-        Modifier.absolutePadding(top = 20.dp)
-    ) {
-        TabRow(
-            modifier = Modifier
-                .absolutePadding(left = 10.dp, right = 10.dp)
-                .clip(RoundedCornerShape(5.dp)),
-            selectedTabIndex = tabIndex,
-            indicator = { tabPositions -> // 3.
-
-            }) {
-            tabs.forEachIndexed { index, tab ->
+    TabRow(selectedTabIndex = currentCategoryTabIndex) {
+            categoryTabs.forEachIndexed { index, categoryTab ->
                 Tab(
-//                    selectedContentColor = MaterialTheme.colors.onPrimary,
-//                    unselectedContentColor = MaterialTheme.colors.onPrimary,
-                    selected = false,
+                    selected = currentCategoryTabIndex == index,
                     onClick = {
-                        currentTab.value = tab
-                        onTabChanged(currentTab.value);
-                        coroutineScope.launch { pagerState.animateScrollToPage(index) }
+                        onTabChanged(categoryTab)
                     },
-                    text = { Text(text = stringResource(id = tab.title)) }
+                    text = { Text(text = stringResource(id = categoryTab.title), maxLines = 2, overflow = TextOverflow.Ellipsis) }
                 )
             }
-        }
-        HorizontalPager( // 4.
-            count = tabs.size,
-            state = pagerState,
-            modifier = Modifier.absolutePadding(left = 10.dp, right = 10.dp)
-        ) { _ ->
-                        CategoryTabItem(
-                            uiState = categoryUiState,
-                            addCategory = {
-                                categoryViewModel.addCategory(it)
-                                categoryViewModel.showSnackBar(SnackBarType.ADD_CATEGORY_SUCCESS)
-                            },
-                            updateCategory = { category,updatedName->
-                                categoryViewModel.updateCategory(category,updatedName)
-                                categoryViewModel.showSnackBar(SnackBarType.UPDATE_CATEGORY_SUCCESS)
-                            },
-                            removeCategory = {
-                                categoryViewModel.removeCategory(it)
-                                categoryViewModel.showSnackBar(SnackBarType.REMOVE_CATEGORY_SUCCESS)
-
-                            },
-                            clearSnackBar = {
-                                categoryViewModel.clearSnackBar()
-                            },
-                            type = type,
-                            bottomSheetScaffoldState = scaffoldState
-                        )
-        }
     }
 }
 
-@OptIn(ExperimentalPagerApi::class, ExperimentalMaterialApi::class)
+@OptIn(ExperimentalPagerApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun ManageCategoryScreen(
     goBack:()->Unit
 ){
     val categoryViewModel = hiltViewModel<CategoryViewModel>()
     val categoryUiState by categoryViewModel.viewModelUiState.collectAsState()
-    val currentCategoryType = remember { mutableStateOf(TransactionType.Income) }
-
-   CategoryTabs(
-       categoryUiState = categoryUiState,
-       categoryViewModel = categoryViewModel,
-       onTabChanged = {
-           if(it.index==1){
-               currentCategoryType.value = TransactionType.Income
-           }else{
-               currentCategoryType.value = TransactionType.Expense
-           }
-       },
-       type = currentCategoryType.value
-   )
-
-
-}
-
-@SuppressLint("UnusedMaterialScaffoldPaddingParameter", "UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalMaterialApi::class, ExperimentalMaterial3Api::class)
-@Composable
-fun CategoryTabItem(
-    uiState: CategoryUiState,
-    clearSnackBar:()->Unit,
-    addCategory:(transactionCategory:TransactionCategory)->Unit,
-    updateCategory:(transactionCategory:TransactionCategory,updatedName:String)->Unit,
-    removeCategory:(transactionCategory:TransactionCategory)->Unit,
-    type:TransactionType = TransactionType.Income,
-    bottomSheetScaffoldState: BottomSheetScaffoldState
-){
-
-    val categories =  uiState.categories.filter { it.transaction_type==type }
-    val isReusableInputDialogShown = remember { mutableStateOf(false) }
+    var currentCategoryTabIndex = remember { mutableStateOf(0) }
     val currentEditingCategory = remember { mutableStateOf<TransactionCategory?>(null) }
-    val coroutineScope = rememberCoroutineScope()
+    val isReusableInputDialogShown = remember { mutableStateOf(false) }
+    val isChooseCategoryActionDialogOpen = remember { mutableStateOf(false) }
+    val isConfirmDeleteCategoryDialogShown = remember { mutableStateOf(false) }
 
+    val categories =  categoryUiState.categories.filter { it.transaction_type==if(currentCategoryTabIndex.value==0) TransactionType.Expense else TransactionType.Income }
+
+
+    fun clearDialogs(){
+        isReusableInputDialogShown.value = false
+        isChooseCategoryActionDialogOpen.value = false
+        isConfirmDeleteCategoryDialogShown.value = false
+    }
+    ChooseCategoryActionDialog(
+        isOpen = isChooseCategoryActionDialogOpen.value && currentEditingCategory.value!=null,
+        onDeleteCategory = {
+            isChooseCategoryActionDialogOpen.value = false
+            isConfirmDeleteCategoryDialogShown.value = true
+        },
+        onEditCategory =  {
+            isChooseCategoryActionDialogOpen.value = false
+            isReusableInputDialogShown.value = true
+        }
+    )
+    if(isConfirmDeleteCategoryDialogShown.value){
+        AppAlertDialog(
+            title= stringResource(id = R.string.r_u_sure),
+            positiveBtnText = stringResource(id = R.string.confirm),
+            negativeBtnText = stringResource(id = R.string.cancel),
+            onPositiveBtnClicked = {
+                currentEditingCategory.value?.let { categoryViewModel.removeCategory(it) }
+                clearDialogs()
+                currentEditingCategory.value = null
+            },
+            onNegativeBtnClicked = {
+                clearDialogs()
+                currentEditingCategory.value = null
+            }
+        ){
+            Text(text = stringResource(id = R.string.r_u_sure_cat_delete) )
+        }
+    }
 
     AddNameInputDialog(
         initialValue = currentEditingCategory.value?.name?:"",
-        title = if(type==TransactionType.Income) stringResource(id = R.string.enter_in_category) else stringResource(R.string.enter_ex_category),
+        title = if(currentCategoryTabIndex.value==0) stringResource(id = R.string.enter_in_category) else stringResource(R.string.enter_ex_category),
         isShown =  isReusableInputDialogShown.value,
-        dialogColor = if(type==TransactionType.Income) positiveColor else negativeColor,
         onDialogDismiss = {
-                            isReusableInputDialogShown.value = false
-                            currentEditingCategory.value = null
-                            coroutineScope.launch {
-                                bottomSheetScaffoldState.bottomSheetState.collapse()
-                            }
-                          },
+            clearDialogs()
+            currentEditingCategory.value = null
+        },
         onConfirmClick = {
-            coroutineScope.launch {
-                bottomSheetScaffoldState.bottomSheetState.collapse()
-            }
+
             isReusableInputDialogShown.value = false
 
             if(currentEditingCategory.value==null){
                 val category = TransactionCategory(
                     id = 0,
                     name = it,
-                    transaction_type = type,
+                    transaction_type = if(currentCategoryTabIndex.value==0) TransactionType.Income else TransactionType.Expense,
                     created_at =  System.currentTimeMillis()
                 )
-                addCategory(category)
+                categoryViewModel.addCategory(category)
             }else{
-                    updateCategory(currentEditingCategory.value!!,it)
+                categoryViewModel.updateCategory(currentEditingCategory.value!!,it)
             }
-
+            clearDialogs()
             currentEditingCategory.value = null
-
         }
     )
 
     Scaffold(
-        floatingActionButton = {
-            if (!bottomSheetScaffoldState.bottomSheetState.isAnimationRunning && bottomSheetScaffoldState.bottomSheetState.isCollapsed) {
-                Box(
-                    modifier = Modifier
-                        .absolutePadding(bottom = 100.dp, left = 30.dp)
+        topBar =  {
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+
                 ) {
-                    TextButton(
-                        onClick = {
-                            currentEditingCategory.value = null
-                            isReusableInputDialogShown.value = true
-                        },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clip(RoundedCornerShape(8.dp))
-                            .padding(0.dp)
-                            .background(if (type == TransactionType.Income) positiveColor else negativeColor)
-                    ) {
-                        Text(
-                            text = if (type == TransactionType.Income) stringResource(id = R.string.add_income_cat) else stringResource(
-                                R.string.add_expense_cat
-                            )
-                        )
-                        Icon(
-                            modifier = Modifier
-                                .width(30.dp)
-                                .height(30.dp),
-                            imageVector = Icons.Filled.Add, contentDescription = "add category"
-                        )
+                    Icon(Icons.Filled.Home, contentDescription = "s")
+                    Text(
+                        "X Money Tracker",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                }
+                if(currentCategoryTabIndex.value==0){
+                    OutlinedButton(onClick = {
+                        currentEditingCategory.value = null
+                        isReusableInputDialogShown.value = true
+                    }) {
+                        Text(text = stringResource(id = R.string.add_income_cat))
+                        Icon(imageVector = Icons.Filled.Add, contentDescription ="" )
+                    }
+                }else{
+                    OutlinedButton(onClick = {
+                        currentEditingCategory.value = null
+                        isReusableInputDialogShown.value = true
+                    }) {
+                        Text(text = stringResource(id = R.string.add_expense_cat))
+                        Icon(imageVector = Icons.Filled.Add, contentDescription ="" )
                     }
                 }
             }
-        }) {
-
+        }
+    ) {
+         Column(Modifier.padding(it)) {
+             CategoryTabs(
+                 onTabChanged = {
+                     currentCategoryTabIndex.value = it.index
+                 },
+                 currentCategoryTabIndex = currentCategoryTabIndex.value
+             )
+             LazyColumn(Modifier.padding(10.dp)){
+                 items(categories){
+                        CategoryItem(
+                            category = it,
+                            onClick = {
+                                    isChooseCategoryActionDialogOpen.value = true
+                                    currentEditingCategory.value = it
+                            }
+                        )
+                 }
+             }
+         }
     }
 
-//        Text(text = if(type===TransactionType.Income) "Income Categories List :" else "Expense Categories List :", style = MaterialTheme.typography.h6, modifier = Modifier.absolutePadding(top = 30.dp, bottom = 20.dp))
 
+}
 
-    }
-//    LazyColumn(
-//        verticalArrangement = Arrangement.Top,
-//        modifier = Modifier.absolutePadding(left = 2.dp, right = 2.dp, bottom = 100.dp, top = 20.dp)
-//    ) {
-//        items(categories){
-//            CategoryItem(
-//                category = it,
-//                itemColor =  if(type==TransactionType.Income) positiveColor else negativeColor,
-//                onClick = {
-//                    coroutineScope.launch {
-//                        currentEditingCategory.value = it
-//                        bottomSheetScaffoldState.bottomSheetState.expand()
-//                    }
-//                }
-//            )
-//        }
-//    }
-//    Column(
-//        modifier= Modifier
-//            .height(200.dp)
-//            .padding(20.dp),
-//        verticalArrangement = Arrangement.Center,
-//        horizontalAlignment = Alignment.CenterHorizontally
-//    ) {
-//        TextButton(
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .clip(RoundedCornerShape(5.dp))
-//                .padding(0.dp)
-//                .background(primary)
-//            ,
-//            onClick = {
-//                isReusableInputDialogShown.value = true
-//            }
-//        ) {
-//            Text(text = stringResource(id = R.string.edit_tran))
-//        }
-//        Divider(Modifier.height(20.dp), color = Color.Transparent)
-//        TextButton(
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .clip(RoundedCornerShape(5.dp))
-//                .padding(0.dp)
-//                .background(Red300)
-//            ,
-//            onClick = {
-//                currentEditingCategory.value?.let { removeCategory(it) }
-//            }
-//        ) {
-//            Text(text = stringResource(id = R.string.delete_tran))
-//        }
-//    }
-//    SuBuuSnackBar(
-//        onDismissSnackBar = { clearSnackBar() },
-//        scaffoldState = bottomSheetScaffoldState,
-//        snackBarType = uiState.currentSnackBar,
-//    )
-
-
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun CategoryItem(
     modifier: Modifier=Modifier,
     category:TransactionCategory,
-    itemColor:Color,
     onClick:()->Unit
 ){
         Card(
-            elevation = 2.dp,
-            contentColor = itemColor,
-            modifier = modifier,
-            onClick = {
-                onClick()
-            }
+            modifier = modifier.clickable { onClick() }
         ) {
             Column(
                 Modifier
@@ -326,6 +222,5 @@ fun CategoryItem(
                 Text(text = category.name)
             }
         }
-    Divider(Modifier.height(3.dp), color = Color.Transparent)
-
+          Divider(Modifier.height(3.dp), color = Color.Transparent)
 }
