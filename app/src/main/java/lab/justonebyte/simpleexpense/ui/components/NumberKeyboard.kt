b9117a014850019d242f4853723a27d3010a2bba
lwinmoehein <dev.lwinmoehein@gmail.com>
2024-01-12 13:32:18 +0630

    - improve number keyboard

--- app/src/main/java/lab/justonebyte/simpleexpense/ui/components/NumberKeyboard.kt
+++ app/src/main/java/lab/justonebyte/simpleexpense/ui/components/NumberKeyboard.kt
@@ -5,16 +5,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absolutePadding
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -26,17 +27,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import compose.icons.FeatherIcons
 import compose.icons.feathericons.Check
+import compose.icons.feathericons.CheckSquare
 import compose.icons.feathericons.Delete
+import lab.justonebyte.simpleexpense.R
+import lab.justonebyte.simpleexpense.model.Currency
 
 @Composable
 fun NumberKeyboard(
-    onNumberClick: (Int) -> Unit,
-    onDeleteClick: () -> Unit,
-    onOkClick: () -> Unit
+    onNumberConfirm: (Int) -> Unit,
+    currency: Currency
 ) {
     var currentNumber by remember { mutableStateOf("") }
 
@@ -49,47 +53,84 @@ fun NumberKeyboard(
     ) {
         Row(
             modifier = Modifier
-                .padding(vertical = 16.dp)
                 .fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
 
             Card(
+                modifier = Modifier.height(50.dp)
             ) {
-                Text(
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(5.dp),
-                    text = currentNumber,
-                    style = MaterialTheme.typography.titleLarge,
-                    color = MaterialTheme.colorScheme.primary,
-                    fontWeight = FontWeight.ExtraBold
-                )
-            }
-            IconButton(onClick = {
-
-            }) {
-                Icon( imageVector = FeatherIcons.Delete, contentDescription = null, tint = MaterialTheme.colorScheme.primary)
+                        .padding(horizontal = 8.dp, vertical = 10.dp),
+                ) {
+                    Row(
+                        Modifier.padding(horizontal = 10.dp)
+                    ) {
+                        Text(
+                            modifier = Modifier.absolutePadding(right = 15.dp),
+                            text = stringResource(id =if(currency.name==Currency.Kyat.name) R.string.kyat else R.string.dollar),
+                            color = MaterialTheme.colorScheme.onSurface,
+                            style = MaterialTheme.typography.titleLarge
+                        )
+                        Spacer(modifier = Modifier
+                            .fillMaxHeight()
+                            .width(2.dp)
+                            .background(MaterialTheme.colorScheme.secondary))
+                    }
+                    Text(
+                        text = if(currentNumber.isEmpty()) stringResource(id = R.string.enter_amount) else currentNumber,
+                        style = if(currentNumber.isEmpty()) MaterialTheme.typography.titleSmall else MaterialTheme.typography.titleLarge,
+                        color =if(currentNumber.isEmpty()) MaterialTheme.colorScheme.secondary else  MaterialTheme.colorScheme.primary,
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                }
             }
         }
 
         NumberButtonRow(
             numbers = listOf(1, 2, 3,10),
             onNumberClick = {
-                currentNumber += it.toString()
+                if(!(currentNumber.isEmpty() && it==0)){
+                    currentNumber += it.toString()
+                }
+            },
+            onDelete = {
+                currentNumber = currentNumber.dropLast(1)
+            },
+            onConfirm = {
+                onNumberConfirm(currentNumber.toInt())
             }
         )
         NumberButtonRow(
             numbers = listOf(4, 5, 6,0),
             onNumberClick = {
-                currentNumber += it.toString()
+                if(!(currentNumber.isEmpty() && it==0)){
+                    currentNumber += it.toString()
+                }
+            },
+            onDelete = {
+                currentNumber = currentNumber.dropLast(1)
+            },
+            onConfirm = {
+                onNumberConfirm(currentNumber.toInt())
             }
         )
         NumberButtonRow(
             numbers = listOf(7, 8, 9,11),
             onNumberClick = {
-                currentNumber += it.toString()
+                if(!(currentNumber.isEmpty() && it==0)){
+                    currentNumber += it.toString()
+                }
+            },
+            onDelete = {
+                currentNumber = currentNumber.dropLast(1)
+            },
+            onConfirm = {
+                onNumberConfirm(currentNumber.toInt())
             }
         )
 
@@ -118,7 +159,9 @@ fun NumberKeyboard(
 private fun NumberButtonRow(
     modifier: Modifier = Modifier,
     numbers: List<Int>,
-    onNumberClick: (Int) -> Unit
+    onNumberClick: (Int) -> Unit,
+    onDelete:()->Unit,
+    onConfirm:()->Unit
 ) {
     Row(
         modifier = modifier.fillMaxWidth(),
@@ -144,7 +187,9 @@ private fun NumberButtonRow(
                 ) {
                     IconButton(
                         modifier = Modifier.fillMaxSize(),
-                        onClick = { onNumberClick(number) }
+                        onClick = {
+                            onDelete()
+                        }
                     ) {
                         Row (
                             modifier = Modifier.fillMaxSize(),
@@ -168,7 +213,9 @@ private fun NumberButtonRow(
                 ) {
                     IconButton(
                         modifier = Modifier.fillMaxSize(),
-                        onClick = { onNumberClick(number) }
+                        onClick = {
+                            onConfirm
+                        }
                     ) {
                         Row (
                             modifier = Modifier.fillMaxSize(),
