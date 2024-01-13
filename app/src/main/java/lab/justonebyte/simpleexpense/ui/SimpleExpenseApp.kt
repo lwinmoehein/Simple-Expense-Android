7a5c1c86149708e971313426ce3949467336257e
lwinmoehein <dev.lwinmoehein@gmail.com>
2024-01-13 14:38:12 +0630

    - add showcase strings

--- app/src/main/java/lab/justonebyte/simpleexpense/ui/SimpleExpenseApp.kt
+++ app/src/main/java/lab/justonebyte/simpleexpense/ui/SimpleExpenseApp.kt
@@ -2,13 +2,8 @@ package lab.justonebyte.simpleexpense.ui
 
 import AppTheme
 import android.content.Intent
-import android.os.Build
 import androidx.activity.result.ActivityResultLauncher
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
@@ -20,7 +15,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -33,7 +27,6 @@ import com.canopas.lib.showcase.IntroShowcase
 import com.canopas.lib.showcase.component.ShowcaseStyle
 import com.google.accompanist.insets.ProvideWindowInsets
 import compose.icons.FeatherIcons
-import compose.icons.feathericons.ArrowRight
 import compose.icons.feathericons.Home
 import compose.icons.feathericons.List
 import compose.icons.feathericons.PieChart
