836060857624ce158e79ad04be064c00c4831437
lwinmoehein <dev.lwinmoehein@gmail.com>
2024-01-18 14:42:14 +0630

    - add no data image

--- app/src/main/java/lab/justonebyte/simpleexpense/ui/home/NoData.kt
+++ app/src/main/java/lab/justonebyte/simpleexpense/ui/home/NoData.kt
@@ -1,18 +1,24 @@
 package lab.justonebyte.simpleexpense.ui.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import compose.icons.FeatherIcons
 import compose.icons.feathericons.Info
@@ -27,12 +33,16 @@ fun NoData(modifier: Modifier=Modifier,text:String= stringResource(id = R.string
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ){
-        Row (
-            horizontalArrangement = Arrangement.spacedBy(5.dp),
-            verticalAlignment = Alignment.CenterVertically
+        Column (
+           horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ){
-            Icon(imageVector = FeatherIcons.Info, contentDescription ="",tint=MaterialTheme.colorScheme.primary)
-            Text(text = text, color = MaterialTheme.colorScheme.secondary)
+            Image(
+                modifier = Modifier.width(180.dp).height(180.dp),
+                painter = painterResource(id = R.drawable.no_results),
+                contentDescription =""
+            )
+            Text(text = text, color = MaterialTheme.colorScheme.secondary,style=MaterialTheme.typography.labelLarge, fontWeight = FontWeight.Bold)
         }
     }
 }
