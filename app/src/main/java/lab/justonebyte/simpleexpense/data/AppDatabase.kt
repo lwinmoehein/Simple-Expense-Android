b48e623f520cb21118a511876c6ab5fdebaaea3c
lwinmoehein <dev.lwinmoehein@gmail.com>
2024-01-22 14:33:11 +0630

    - remove initial category creation

--- app/src/main/java/lab/justonebyte/simpleexpense/data/AppDatabase.kt
+++ app/src/main/java/lab/justonebyte/simpleexpense/data/AppDatabase.kt
@@ -1,15 +1,11 @@
 package lab.justonebyte.simpleexpense.data
 
 import android.content.Context
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
-import lab.justonebyte.simpleexpense.model.TransactionType
 
 
 // Annotates class to be a Room Database with a table (entity) of the Word class
@@ -37,9 +33,7 @@ abstract class AppDatabase : RoomDatabase() {
                 ).addCallback( object : Callback() {
                     override fun onCreate(db: SupportSQLiteDatabase) {
                                         INSTANCE?.let { database ->
-                    scope.launch {
-                        populateCategories(database.categoryDao())
-                    }
+
                 }
                     }
                 })
@@ -49,17 +43,5 @@ abstract class AppDatabase : RoomDatabase() {
                 instance
             }
         }
-    }
-}
-suspend fun populateCategories(dao: CategoryDao) {
-    dao.let {
-        it.insert(CategoryEntity(name = "မနက်စာ", unique_id ="one", transaction_type = TransactionType.Expense.value, created_at = System.currentTimeMillis(), updated_at = System.currentTimeMillis()))
-        it.insert(CategoryEntity(name = "ကားခ",unique_id = "two", transaction_type = TransactionType.Expense.value, created_at = System.currentTimeMillis(), updated_at = System.currentTimeMillis()))
-        it.insert(CategoryEntity(name = "အိမ်ပေး",unique_id = "three", transaction_type = TransactionType.Expense.value, created_at =  System.currentTimeMillis(), updated_at = System.currentTimeMillis()))
-        it.insert(CategoryEntity(name = "လစာ",unique_id = "Foru", transaction_type = TransactionType.Income.value, created_at =  System.currentTimeMillis(), updated_at = System.currentTimeMillis()))
-        it.insert(CategoryEntity(name = "မုန့်ဖိုး",unique_id ="five", transaction_type = TransactionType.Income.value, created_at =   System.currentTimeMillis(), updated_at = System.currentTimeMillis()))
-        it.insert(CategoryEntity(name = "ဘောနပ်စ်",unique_id = "six",  transaction_type = TransactionType.Income.value,created_at = System.currentTimeMillis(), updated_at = System.currentTimeMillis()))
-        it.insert(CategoryEntity(name = "လက်ဆောင်",unique_id = "seven", transaction_type = TransactionType.Income.value, created_at = System.currentTimeMillis(), updated_at = System.currentTimeMillis()))
-
     }
 }
