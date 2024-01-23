ccf7d454ba33d83c775c252d1964a1f84a22cd24
lwinmoehein <dev.lwinmoehein@gmail.com>
2024-01-23 10:31:32 +0630

    - update icons

--- app/src/main/java/lab/justonebyte/simpleexpense/ui/components/Icons.kt
+++ app/src/main/java/lab/justonebyte/simpleexpense/ui/components/Icons.kt
@@ -4,14 +4,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import compose.icons.FeatherIcons
+import compose.icons.feathericons.Activity
 import compose.icons.feathericons.Award
 import compose.icons.feathericons.Briefcase
 import compose.icons.feathericons.Coffee
 import compose.icons.feathericons.Cpu
+import compose.icons.feathericons.DollarSign
+import compose.icons.feathericons.Home
 import compose.icons.feathericons.Map
 import compose.icons.feathericons.Menu
+import compose.icons.feathericons.MoreVertical
 import compose.icons.feathericons.ShoppingCart
 import compose.icons.feathericons.Smile
+import compose.icons.feathericons.Truck
 import lab.justonebyte.simpleexpense.R
 
 @Composable
@@ -20,13 +25,17 @@ fun getIconFromName(name:String):ImageVector{
         "food_and_drink"->FeatherIcons.Coffee
         "it_devices"->FeatherIcons.Cpu
         "shopping"->FeatherIcons.ShoppingCart
+        "housing"->FeatherIcons.Home
         "travel"->FeatherIcons.Map
         "life_and_entertainment"->FeatherIcons.Smile
+        "vehicles"->FeatherIcons.Truck
         "salary"->FeatherIcons.Briefcase
+        "health"->FeatherIcons.Activity
         "bonus"->FeatherIcons.Award
+        "donation"->FeatherIcons.DollarSign
         "allowance"->ImageVector.vectorResource(
             id = R.drawable.allowance
         )
-        else -> FeatherIcons.Menu
+        else -> FeatherIcons.MoreVertical
     }
 }
